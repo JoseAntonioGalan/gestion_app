72d9d81bddd19010d794b952c9643a9b98dbad50
JoseAntonioGalan <galanmaquedajoseantonio@gmail.com>
2022-05-16 22:50:41 +0200

    Refactorizar codigo validator y eliminar imports

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -1,7 +1,7 @@
 class Validator {
   static String? name(String? name) {
     if (name == null || name.isEmpty) {
-      return 'Name can\'t be empty';
+      return 'Nombre no puede estar vacío';
     }
 
     return null;
@@ -12,9 +12,9 @@ class Validator {
         r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
 
     if (email == null || email.isEmpty) {
-      return 'Email can\'t be empty';
+      return 'Email no puede estar vacío';
     } else if (!emailRegExp.hasMatch(email)) {
-      return 'Enter a correct email';
+      return 'Introduce un email correcto';
     }
 
     return null;
@@ -22,9 +22,9 @@ class Validator {
 
   static String? password(String? password) {
     if (password == null || password.isEmpty) {
-      return 'Password can\'t be empty';
-    } else if (password.length < 6) {
-      return 'Enter a password with length at least 6';
+      return 'Password no puede estar vacío';
+    } else if (password.length < 8) {
+      return 'La contraseña debe de tener minimo 8 caracteres';
     }
 
     return null;
